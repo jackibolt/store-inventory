f27c4414f78c5609c61c123c05ffb8eb67f9a215
jackibolt <40141575+jackibolt@users.noreply.github.com>
2023-12-29 09:01:42 -0500

    started menu

--- app.py
+++ app.py
@@ -3,6 +3,21 @@ from models import (Base, session,
 import csv, datetime, time
 
 
+def menu():
+    while True:
+        print('''
+              \nSTORE INVENTORY
+              \r
+              \rEnter 'v' to view products
+              \rEnter 'x' to view all products
+              \rEnter 'a' to add a product
+              \rEnter 'b' to export a csv
+              \r''')
+        return input('Selection:  ')
+        
+        
+
+
 def clean_price(price_str):
     price_split = price_str.split('$')
     price_float = float(price_split[1])
@@ -23,13 +38,38 @@ def clean_date(date_str):
     return datetime.date(year, month, day)
 
 
+def clean_selected_id(id_str, options):
+    try:
+        id_str = int(id_str)
+        options_list = []
+        for item in options:
+            option_num = item.split('.')
+            option_num = int(option_num[0])
+            options_list.append(option_num)
+    except ValueError:
+        input('''
+              \n ----- ID ERROR -----
+              \rThe id should be a numerical value.
+              \rPress enter to try again.
+              \r''')
+    else:
+        if id_str in options_list:
+            return id_str
+        else:
+            input('''
+                  \n ----- ID ERROR -----
+                  \rThe id should be a numerical value that corresponds with a product in the inventory.
+                  \rPress enter to try again.
+                  \r''')
+
+
 
 def add_csv():
     with open('inventory.csv') as csv_file:
         data = csv.reader(csv_file)
         next(data)
         for row in data:
-            print(row)
+            # print(row)
             item_in_inventory = session.query(Product).filter(Product.product_name==row[0]).one_or_none()
             if item_in_inventory == None:
                 product_name = row[0]
@@ -43,7 +83,48 @@ def add_csv():
         session.commit()
 
 
+def app():
+    app_running = True
+    while app_running:
+        choice = menu()
+        if choice.lower() == 'v':
+            #view
+            product_list = []
+            for product in session.query(Product):
+                product_list.append(f'{product.product_id}. {product.product_name}')
+            print('\nProduct List: ')
+            for item in product_list:
+                print(f'\r{item}')
+
+            id_error = True
+            while id_error:
+                selected_id = input('Select product id:  ')
+                selected_id = clean_selected_id(selected_id, product_list)
+                if type(selected_id) == int:
+                    id_error = False
+            selected_product = session.query(Product).filter(Product.product_id == selected_id).first()
+            print(selected_product)
+
+
+        elif choice.lower() == 'x':
+            for product in session.query(Product):
+                print(f'''\n{product.product_id}. {product.product_name} | ${product.product_price/100} | x{product.product_quantity}
+                     \rLast Updated: {product.date_updated}''')
+            input('\nPress enter to return to the main menu')
+        elif choice.lower() == 'a':
+            #add
+            pass
+        elif choice.lower() == 'b':
+            #export csv
+            pass
+        else:
+            print("Goodbye")
+            app_running = False
+
+
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
     add_csv()
+    app()
+    
